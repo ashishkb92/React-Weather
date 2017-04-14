013f8f211df6bd9f008d018ade1e6a712099fb35
ashishkb92 <ashishkb92@gmail.com>
2017-04-15 00:23:53 +0530

    Add foundation

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,49 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   resolve: {
-  	root: __dirname,
-  	alias:{
-      Main:'app/Components/Main.jsx',
-  	  Nav:'app/Components/Nav.jsx', 
-      Weather:'app/Components/Weather.jsx',
-      WeatherForm:'app/Components/WeatherForm.jsx',
-      WeatherMessage:'app/Components/WeatherMessage.jsx',
-      About:'app/Components/About.jsx',
-      Examples:'app/Components/Examples.jsx',
-      openWeatherMap :'app/api/openWeatherMap.jsx'
-  	}, 
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
+      openWeatherMap: 'app/api/openWeatherMap.jsx'
+    },
     extensions: ['', '.js', '.jsx']
   },
-
- module :{
-
- 	loaders: [
- 	{
-     loader:'babel-loader', 
-     query : {
-        presets :['react','es2015','stage-0']
-
-     },
-      
-     test : /\jsx?$/,
-     exclude : /(node_modules|bower_components)/
-
- 	}
-
- 	]
- },
- devtool :'cheap-module-eval-source-map'
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
