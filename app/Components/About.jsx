dc379fcc5ffe8d2515d9b6d4418572e803e69bbe
ashishkb92 <ashishkb92@gmail.com>
2017-04-15 12:04:18 +0530

    Add about

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -4,12 +4,7 @@ var React = require('react');
 
  render : function (){
 
-    return (
-
-       <h2>About Component</h2>
-
-
-    	);
+    
 
  }
 
@@ -17,7 +12,24 @@ var React = require('react');
 
 }); */
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+
+     return (
+       <div>
+       <h1 className = 'text-cener'>About</h1>
+       <p>This is an app build by me</p>
+       <p>Here are some tools i used.</p>
+       <ul> 
+           <li> <a href = "http://facebook.github.io/react">React</a> - This was the javascript framework used
+           </li>
+           <li>
+           <a href ="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+           </li>
+       </ul>
+       </div>
+    	);
+
+};
 
 
 module.exports = About;
